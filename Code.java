6df9b433bc0b72357dba3a2a7627de15abe2731c
Pradeep Sahani <92988101+Sahanipradeep@users.noreply.github.com>
2021-12-11 12:52:22 +0530

    Create Code.java

---
+++ Code.java
@@ -0,0 +1,26 @@
+public class MyClass {
+    public static void main(String args[]) {
+      
+		int a=10,b=20,c=30;
+		int result;
+		if (a>b && a>c) 
+		{
+			result=a;
+		}
+		else if(b>a && b>c)
+		{
+			result=b;
+		}
+		else
+		{
+			result=c;
+		}
+		System.out.println(result);
+		
+	}
+
+	
+
+
+    
+}
